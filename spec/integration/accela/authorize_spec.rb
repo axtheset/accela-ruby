e566e38bf2c9d2341aeb916d95ca16b5668bd319
Branden Wiegand <branden@brandenwiegand.com>
2014-07-30 11:20:19 -0400

    Added nil case to authorize spec.

--- spec/integration/accela/authorize_spec.rb
+++ spec/integration/accela/authorize_spec.rb
@@ -1,23 +1,31 @@
 require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
 describe Accela::Authorize, :vcr do
-  it "returns an access token, a refresh token and additional information" do
-    app_id = "635395466279594650"
-    app_secret ="3b1e4026d95e4478a0f8dd1f7a1b7028"
-    agency = "ISLANDTON-APP"
-    environment = "TEST"
+  let(:app_id) { "635395466279594650" }
+  let(:app_secret) {"3b1e4026d95e4478a0f8dd1f7a1b7028" }
+  let(:agency) { "ISLANDTON-APP" }
+  let(:environment) { "TEST" }
+  let(:username) { "developer" }
+  let(:password) { "accela" }
+  let(:scope) { "records addresses" }
 
-    username = "developer"
-    password = "accela"
-    scope = "records addresses"
+  let(:auth) { Accela::Authorize.new(app_id, app_secret, agency, environment) }
+  let(:token) { auth.login(username, password, scope) }
 
-    auth = Accela::Authorize.new(app_id, app_secret, agency, environment)
-    token = auth.login(username, password, scope)
+  context "valid credentials" do
+    it "returns an access token, a refresh token and additional information" do
+      expect(token.access_token).to be_truthy
+      expect(token.token_type).to be_truthy
+      expect(token.expires_in).to be_truthy
+      expect(token.refresh_token).to be_truthy
+      expect(token.scope).to be_truthy
+    end
+  end
 
-    expect(token.access_token).to be_truthy
-    expect(token.token_type).to be_truthy
-    expect(token.expires_in).to be_truthy
-    expect(token.refresh_token).to be_truthy
-    expect(token.scope).to be_truthy
+  context "invalid credentials" do
+    let(:password) { "oops" }
+    it "returns nil" do
+      expect(token).to be_nil
+    end
   end
 end
