54f7bb44d9ef3fa1b113f39653bcc2594cfcfb22
Branden Wiegand <branden@brandenwiegand.com>
2014-07-31 10:16:36 -0400

    Oops. Added missing assertions.

--- spec/integration/accela/api_spec.rb
+++ spec/integration/accela/api_spec.rb
@@ -8,12 +8,14 @@ describe Accela::API, :vcr do
 
     it "works with no_auth authentication" do
       response = api.get("/v4/records", :no_auth)
+      expect(response.code).to eq 200
     end
 
     it "works with access_token authentication" do
       id = "ISLANDTON-DUB14-00000-0000H"
       api.login("developer", "accela", "records addresses")
       response = api.get "/v4/records/#{id}", :access_token
+      expect(response.code).to eq 200
     end
 
   end
