2c8954950d4936f6674c64ca14df8afd02e5a3d5
Branden Wiegand <branden@brandenwiegand.com>
2014-08-15 16:54:49 -0400

    Update attributes on models.

--- lib/accela/model.rb
+++ lib/accela/model.rb
@@ -6,13 +6,32 @@ module Accela
       @raw = raw
     end
 
-    def method_missing(name)
-      property = name.to_sym
-      if raw.has_key?(property)
-        raw.fetch(property)
+    def method_missing(name, *args, &block)
+      if is_property?(name)
+        value_for_property(name)
+      elsif is_assignment?(name)
+        set_value_for_property(name.to_s.gsub("=", ""), args.first)
       else
         super
       end
+    end
+
+    private
+
+    def set_value_for_property(name, value)
+      raw[name.to_sym] = value
+    end
+
+    def value_for_property(name)
+      raw.fetch(name.to_sym)
+    end
+
+    def is_property?(name)
+      raw.has_key?(name.to_sym)
+    end
+
+    def is_assignment?(name)
+      name.to_s =~ /=$/
     end
 
   end
--- spec/integration/accela/record_spec.rb
+++ spec/integration/accela/record_spec.rb
@@ -2,10 +2,13 @@ require File.expand_path(File.dirname(__FILE__) + "/../spec_helper")
 
 describe Accela::Record, :vcr do
 
+  before(:each) do
+    api = Accela::API.connection
+    api.login("developer", "accela", "records addresses")
+  end
+
   describe "::find" do
     it "returns a single Record object" do
-      api = Accela::API.connection
-      api.login("developer", "accela", "records addresses")
       record = Accela::Record.find("ISLANDTON-DUB14-00000-0000H")
       expect(record.initiated_product).to eq "AV360"
       expect(record.public_owned).to eq false
@@ -14,10 +17,24 @@ describe Accela::Record, :vcr do
 
   describe "::all" do
     it "returns a list of Record objects" do
-      api = Accela::API.connection
-      api.login("developer", "accela", "records addresses")
       records = Accela::Record.all
       expect(records.length).to eq 13
+    end
+  end
+
+  describe "updating a record's attributes" do
+    it "allows its properties to be updated" do
+      record = Accela::Record.find("ISLANDTON-DUB14-00000-0000H")
+      record.name = "This is a test"
+      expect(record.name).to eq "This is a test"
+    end
+
+    it "does not persist changes" do
+      record = Accela::Record.find("ISLANDTON-DUB14-00000-0000H")
+      record.module = "This is a test"
+      expect(record.module).to eq "This is a test"
+      record_take_2 = Accela::Record.find("ISLANDTON-DUB14-00000-0000H")
+      expect(record_take_2.module).not_to eq "This is a test"
     end
   end
 
