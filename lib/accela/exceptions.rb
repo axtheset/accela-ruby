b5abd9c377cefa6d54bc369acec59f0a004b1d71
Branden Wiegand <branden@brandenwiegand.com>
2014-08-19 16:46:42 -0400

    Better Error handling.

--- lib/accela/exceptions.rb
+++ lib/accela/exceptions.rb
@@ -1,11 +1,15 @@
 module Accela
   class AccelaError < ::StandardError; end
 
+  # HTTP API Errors
   class InvalidTokenError < AccelaError; end
-
   class AuthorizationError < AccelaError; end
+  class ForbiddenError < AccelaError; end
+  class NotFoundError < AccelaError; end
+  class ServerError < AccelaError; end
+  class BadRequestError < AccelaError; end
+  class DataValidationError < AccelaError; end
   class UnsupportedAuthTypeError < AccelaError; end
-
   class UnexpectedError < AccelaError; end
 
   class ConfigurationError < AccelaError
