1613232279f57735d51a95d8eb9a051b92139706
Branden Wiegand <branden@brandenwiegand.com>
2014-08-26 16:57:11 -0400

    Records now allow expansion at API level.

--- lib/accela/api/v4/get_records.rb
+++ lib/accela/api/v4/get_records.rb
@@ -2,9 +2,35 @@ module Accela
   module V4
     class GetRecords < Base
 
-      def call(*ids)
+      def call(*i, q)
+        given_hash = q.class == Hash
+        query = given_hash ? q : {}
+        ids = given_hash ? i : i + [q]
+        query = compact_query(query)
+
         ids_string = ids.flatten.map(&:to_s).join(",")
-        get("records/#{ids_string}", :access_token)
+        get("records/#{ids_string}", :access_token, query)
+      end
+
+      private
+
+      def compact_query(query)
+        Hash[
+          query.map {|key, val|
+            [key.to_s, transform(key).call(val)]
+          }
+        ]
+      end
+
+      def transform(name)
+        map = {
+          expand: ->(i) { i.join(",") }
+        }
+        if map.has_key?(name)
+          map.fetch(name)
+        else
+          ->(i) { i }
+        end
       end
 
     end
--- spec/integration/accela/v4/get_records_spec.rb
+++ spec/integration/accela/v4/get_records_spec.rb
@@ -20,5 +20,13 @@ describe Accela::V4::GetRecords, :vcr do
       expect(payload["result"].length).to eq 2
     end
 
+    it "expands addresses" do
+      api = Accela::API.connection
+      api.login("mdeveloper", "accela", "records addresses")
+      id1 = "ISLANDTON-14CAP-00000-000CR"
+      payload = Accela::V4::GetRecords.call(id1, expand: ["addresses"])
+      expect(payload["result"].first["addresses"].length).to eq 1
+    end
+
   end
 end
