54ff3398784c54bf0c0463c73b3bae28273356de
Branden Wiegand <branden@brandenwiegand.com>
2014-09-03 10:13:00 -0400

    Small simplification.

--- lib/accela/model.rb
+++ lib/accela/model.rb
@@ -15,8 +15,7 @@ module Accela
         unless @raw.has_key?(property)
           raise UnknownAttributeError, "unknown attribute: #{property}"
         else
-          method = "#{property}="
-          public_send(method, value)
+          @raw[property.to_sym] = value
         end
       }
     end
@@ -36,9 +35,9 @@ module Accela
        elsif is_assignment?(name)
          property = name.to_s.gsub("=", "")
          value = args.first
-         if has_one?(property) && !value.kind_of?(Hash) && value
+         if has_one?(property) && value
            value = value.raw
-         elsif has_many?(property) && !value.inject(true) {|m, v| m && v.kind_of?(Hash) } && value
+         elsif has_many?(property) && value
            value = value.map(&:raw)
          end
          set_value_for_property(property, value)
